d8839d1a9c62883443e787d77c7f7af070ac855b
spaceforestchu <spaceforestchu@gmail.com>
2020-05-10 18:21:36 -0400

    fix cors and backend issue

--- routes/coins/controller/coinController.js
+++ routes/coins/controller/coinController.js
@@ -18,19 +18,26 @@ module.exports = {
   },
   getCoinPrices: async (req, res) => {
     let returnData = [];
-
+    let undefinedData = [];
     for (let i = 0; i < req.body.length; i++) {
       try {
         let priceData = await cc.priceFull(req.body[i], "USD");
-        returnData.push(priceData);
+        // console.log("----");
+        // console.log(priceData[Object.keys(priceData)]);
+
+        if (Object.keys(priceData).length === 0) {
+          undefinedData.push(req.body[i]);
+        } else {
+          returnData.push(priceData);
+        }
       } catch (e) {
-        res.status(500).json({
-          error,
-        });
+        continue;
       }
     }
-    res.status(200).json({
+
+    return res.status(200).json({
       priceData: returnData,
+      undefinedData: undefinedData,
     });
   },
 };
